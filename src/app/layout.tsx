17c92301abced6ff8e101a366cc13f1062781cd4
LEEVANCEE <vancee777@outlook.com>
2025-04-25 00:54:10 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next';
 // Supports weights 100-900
-//import '@fontsource-variable/roboto';
-import { Nunito } from 'next/font/google';
-import { PT_Sans } from 'next/font/google';
+import '@fontsource-variable/roboto';
+
 import './globals.css';
 import { Toaster } from '@/components/ui/sonner';
 
@@ -11,17 +10,6 @@ export const metadata: Metadata = {
   description: 'OGKIFY',
 };
 
-const nunito = Nunito({
-  variable: '--font-nunito',
-  subsets: ['latin'],
-});
-
-const ptSans = PT_Sans({
-  variable: '--font-pt-sans',
-  subsets: ['latin'],
-  weight: ['400', '700'],
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -29,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${nunito.variable} ${ptSans.variable} font-sans`}>
+      <body>
         <Toaster />
         {children}
       </body>
